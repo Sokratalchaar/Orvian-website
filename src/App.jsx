55641f31bfefe762bc6d500f62b27abe9db80562
Your Name <your@email.com>
2026-06-16 21:23:55 +0300

    Update Facebook OAuth URL with client ID 986892497656949

--- src/App.jsx
+++ src/App.jsx
@@ -434,7 +434,7 @@ function App() {
               </div>
             ) : (
               <a
-                href="https://www.facebook.com/v19.0/dialog/oauth?client_id=1901500510520836&redirect_uri=https://orvian.me/&scope=pages_show_list,pages_manage_metadata,instagram_basic,instagram_manage_messages,instagram_manage_comments&response_type=code"
+                href="https://www.facebook.com/v19.0/dialog/oauth?client_id=986892497656949&redirect_uri=https://orvian.me/&scope=pages_show_list,pages_manage_metadata,instagram_basic,instagram_manage_messages,instagram_manage_comments&response_type=code"
                 className="instagram-connect-btn"
                 target="_blank"
                 rel="noopener noreferrer"
